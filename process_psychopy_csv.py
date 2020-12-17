96641689f6e424cf60979694169cc7f4813be332
dnaligase <60392997+dnaligase@users.noreply.github.com>
2020-12-17 03:26:08 +0300

    quick data cleaning
    Extract only necessary data columns from PsychoPy .csv the way you want.

---
+++ process_psychopy_csv.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[0]:
+
+
+import os
+import glob
+
+import pandas as pd
+import argparse
+
+from datetime import datetime
+
+
+# In[1]:
+
+
+filedir = os.path.abspath('')
+
+cols_to_extract = ["correct_smell", "correct_symbol", "image_for_study", "correct_key", "correct_key_test",
+ "key_resp_test_trial.keys", "latency", "latency_wo_fixation", "name", "age", "sex", "isMenPhase", "session", "date", "expName",
+ "psychopyVersion", "frameRate"]
+
+
+# In[2]:
+
+
+def process_psychopy_output(args):
+    output_dir = os.path.join(filedir, args.output_dir)
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir) # create new folder in the same as input data directory
+    
+    if args.names:
+        fnames = args.names
+    else:
+        input_dir = os.path.join("data", "*.csv")
+        fnames = glob.glob(input_dir) # parse all the .csv files in data folder
+    
+    dfs = {}
+    for i, name in enumerate(fnames):
+        df = pd.read_csv(fnames[i])
+        df.insert(df.columns.get_loc("latency") + 1, "latency_wo_fixation",
+        df.latency - args.fixation_duration)
+        df = df.loc[:, cols_to_extract]
+        dfs[name.split("/")[-1]] = df[df.correct_smell.notna()]
+    
+
+    if args.concatenate:
+        result = pd.concat(dfs)
+        result.to_csv(os.path.join(output_dir, "merged" +
+                                   datetime.now().strftime("_%Y-%b-%d_%H%M") + ".csv"))
+    else:
+        for fname, df in dfs.items():
+            df.to_csv(os.path.join(output_dir, "processed" + fname))
+
+# In[3]:
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Turns the PsychoPy .csv output into human-readable format. See 'usage' for further details.")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-c", "--concatenate", action="store_true",
+                        help="Export data, concatenated across subjects")
+    group.add_argument("-s", "--separate", action="store_true",
+                        help="Export separate file for each individual")
+
+    group.add_argument("-n", "--names", nargs="+", help="Allows to enter specific file(s)")
+    parser.add_argument("output_dir", nargs="?", default="data_processed", type=str, help="Dir to save output to (Default: data_processed)")
+    parser.add_argument("fixation_duration", nargs="?", default=5.000000, type=float, help="Fixation point duration (Default: 5.000000)")
+    
+    parser.set_defaults(func=process_psychopy_output)
+    args = parser.parse_args()
+    args.func(args)
+    print(">>>Success!<<<")
+
+
+
